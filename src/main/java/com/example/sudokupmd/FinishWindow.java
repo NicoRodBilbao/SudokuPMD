65da68a6d82b5cbc711cc738abdbf599d677d5cc
joana-renteria <joana.rente6@tuta.io>
2022-11-07 12:22:01 +0100

    create empty methods

--- src/main/java/com/example/sudokupmd/FinishWindow.java
+++ src/main/java/com/example/sudokupmd/FinishWindow.java
@@ -3,11 +3,35 @@ package com.example.sudokupmd;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.Time;
+
 public class FinishWindow extends AppCompatActivity {
+
+    private String currentDifficulty;
+    private String currentUsername;
+    private Time currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_window);
     }
+
+    //TODO
+    private void fetchData() {
+
+    }
+
+    private void uploadData() {
+
+    }
+
+    private void populateTable() {
+
+    }
+
+    private void changeDifficulty() {
+
+    }
+
 }
