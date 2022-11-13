544c00ecc07dabf3cd6ccbbaa01f75d0e68df945
joana-renteria <joana.rente6@tuta.io>
2022-11-13 16:30:19 +0100

    difficulty selection spinner and populate table

--- src/main/java/com/example/sudokupmd/FinishWindow.java
+++ src/main/java/com/example/sudokupmd/FinishWindow.java
@@ -2,20 +2,25 @@ package com.example.sudokupmd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Time;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import com.example.data.DbHelper;
 import com.example.data.Score;
 
-public class FinishWindow extends AppCompatActivity {
+public class FinishWindow extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private String currentDifficulty;
     private String currentUsername;
@@ -26,12 +31,18 @@ public class FinishWindow extends AppCompatActivity {
 
     private ArrayList<Score> scores;
 
+    TableLayout tbl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_window);
 
-        currentDifficulty = "Easy";
+        tbl = (TableLayout) findViewById(R.id.tblClasificacion);
+
+        currentDifficulty = "Facil";
+        Spinner difficultySpinner = (Spinner) findViewById(R.id.difficulty_selector);
+        difficultySpinner.setOnItemSelectedListener(this);
 
         scores = new ArrayList<Score>();
 
@@ -72,7 +83,9 @@ public class FinishWindow extends AppCompatActivity {
     /**
      * Fill out the table
      */
+    @SuppressLint("DefaultLocale")
     private void populateTable() {
+        // Filter the results according to difficulty
         ArrayList<Score> tableScores = new ArrayList<>();
         for (Score s : scores) {
             if(tableScores.size() >= 10)
@@ -80,10 +93,53 @@ public class FinishWindow extends AppCompatActivity {
             if (s.getDifficulty().equals(currentDifficulty))
                 tableScores.add(s);
         }
+        // Fill out the table with the results
+        for (int i = 0; i < tableScores.size(); i++) {
+            TableRow tr = new TableRow(this);
+            TextView tx = new TextView(this);
+            tx.setText(
+                    String.format("%d. %s - %s",
+                            i + 1,
+                            tableScores.get(i).getName(),
+                            tableScores.get(i).getCompletion_time()));
+            tr.addView(tx);
+            tbl.addView(tr);
+        }
+        // If there are no scores to display show a message
+        if(tableScores.size() == 0) {
+            TableRow tr = new TableRow(this);
+            TextView tx = new TextView(this);
+            tx.setText(R.string.there_are_no_saved_scores_for_this_difficulty);
+            tr.addView(tx);
+            tbl.addView(tr);
+        }
     }
 
-    private void changeDifficulty() {
+    /**
+     * Change the difficulty of the scores to be shown
+     */
+    private void changeDifficulty(String difficulty) {
+        this.currentDifficulty = difficulty;
         this.populateTable();
     }
 
+    /**
+     * @param parent   The AdapterView where the selection happened
+     * @param view     The view within the AdapterView that was clicked
+     * @param position The position of the view in the adapter
+     * @param id       The row id of the item that is selected
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        this.changeDifficulty(parent.getItemAtPosition(position).toString());
+    }
+
+    /**
+     * @param parent The AdapterView that now contains no selected item.
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Default difficulty
+        this.changeDifficulty("Facil");
+    }
 }
