23520ef88c45f1bb4ce7d344b2c721f7b3112549
2dam <2dam@TartangaLH.eus>
2022-11-14 13:50:10 +0100

    Main window 'done'

--- src/main/java/com/example/sudokupmd/MainWindow.java
+++ src/main/java/com/example/sudokupmd/MainWindow.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
-public class MainWindow extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainWindow extends AppCompatActivity{
     private Button startButton = null;
     private ImageButton exitButton = null;
     private Spinner menu = null;
-    private ImageButton menuButton = null;
     public static final int DifficultyWindow = 1;
 
     @Override
@@ -23,15 +26,32 @@ public class MainWindow extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startButton = (Button) findViewById(R.id.startButton);
         exitButton = (ImageButton) findViewById(R.id.exitButton);
-        menuButton = (ImageButton) findViewById(R.id.menuButton);
+        menu = (Spinner) findViewById(R.id.spinner);
 
-        menu.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, LaguageEnum.values()));
+
+        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if(menu.getSelectedItem().toString().equalsIgnoreCase("English")){
+                    startButton.setText(R.string.start);
+                }if(menu.getSelectedItem().toString().equalsIgnoreCase(LaguageEnum.ENUM2.toString())){
+                    startButton.setText(R.string.empezar);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                startButton.setText(R.string.empezar);
+            }
+        });
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainWindow.this, DifficultyWindow.class);
-                startActivityForResult(intent, DifficultyWindow);
+                intent.putExtra("language", menu.getSelectedItem().toString());
+                //TODO Add language
+                startActivity(intent);
             }
         }) ;
 
@@ -39,18 +59,6 @@ public class MainWindow extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 android.os.Process.killProcess(android.os.Process.myPid());
-            }
-        });
-
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                menu.showContextMenu();
-                if(menu.getSelectedItem().toString().equalsIgnoreCase(LaguageEnum.ENUM1.toString())){
-                    startButton.setText(R.string.start);
-                }if(menu.getSelectedItem().toString().equalsIgnoreCase(LaguageEnum.ENUM2.toString())){
-                    startButton.setText(R.string.empezar);
-                }
             }
         });
     }
